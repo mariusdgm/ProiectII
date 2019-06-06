45d6dad60856e588648696a61f040d9f47d172f6
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2019-06-06 17:55:37 +0300

    changed databases

--- WebAppProject/WebAppProject/WebService1.asmx.cs
+++ WebAppProject/WebAppProject/WebService1.asmx.cs
@@ -71,6 +71,29 @@ namespace WebAppProject
             return dsCategories;
         }
 
+        [WebMethod(Description = "Returns a dataset products from a category(based on category id)")]
+        public DataSet get_products_from_category(int categId)
+        {
+            DataSet dsCategories;
+            dsCategories = new DataSet();
+
+            SqlConnection myCon = new SqlConnection();
+            // Change the connection string on other computers
+            myCon.ConnectionString = connString;
+            myCon.Open();
+
+            SqlDataAdapter daCategories = new SqlDataAdapter("SELECT t.IdPiesa, t.Nume FROM [Categorii] as c " +
+                                                            "INNER JOIN [Marimi] as m on (c.IdCategorie = m.IdCategorie) " +
+                                                            "INNER JOIN [Table] as t on(c.IdCategorie = t.IdCategorie)" +
+                                                            "WHERE c.IdCategorie = " + categId.ToString()
+                                                            , myCon);
+            daCategories.Fill(dsCategories, "Adapted Data");
+
+            myCon.Close();
+
+            return dsCategories;
+        }
+
         [WebMethod(Description = "Returns a Hello World string")]
         public string HelloWorld()
         {
